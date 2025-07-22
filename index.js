79b50b032422243b3e087ddc26fd411ef59c1bca
dalonso <masdavidalonso@gmail.com>
2025-07-22 18:03:00 +0200

    Refactor scoring and foul handling functions for improved clarity and maintainability

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 let scoreHome = 0
 let scoreGuest = 0
 
+const playerGuest = document.getElementById("player-guest")
+const playerHome = document.getElementById("player-home")
+
 document.getElementById("score-home").textContent = scoreHome
 document.getElementById("score-guest").textContent = scoreGuest
 
@@ -9,28 +12,21 @@ document.getElementById("score-guest").textContent = scoreGuest
 const newScoreHome = document.getElementById("score-home")
 const newScoreGuest = document.getElementById("score-guest")
 
-function incrHome1() {
-   scoreHome += 1
-   newScoreHome.textContent = scoreHome
-   winner()
-}
+///
 
-function incrHome2() {
-   scoreHome += 2
-   newScoreHome.textContent = scoreHome  
-   winner()
-}
-
-function incrHome3() {
-   scoreHome += 3
-   newScoreHome.textContent = scoreHome
+function incrementScore(team, score) {
+   if (team === "home") {
+      scoreHome += score
+      newScoreHome.textContent = scoreHome
+   } 
+   else if (team === "guest") {
+      scoreGuest += score
+      newScoreGuest.textContent = scoreGuest
+   }
    winner()
 }
 
 ///
-
-const playerGuest = document.getElementById("player-guest")
-const playerHome = document.getElementById("player-home")
 
 function winner() {
    if (scoreHome > scoreGuest) {
@@ -63,136 +59,88 @@ function winner() {
 
 ///////////////
 
-function incrGuest1() {
-   scoreGuest += 1
-   newScoreGuest.textContent = scoreGuest
-   winner()
-}
-
-function incrGuest2() {
-   scoreGuest += 2
-   newScoreGuest.textContent = scoreGuest
-   winner()
-}
-
-function incrGuest3() {
-   scoreGuest += 3
-   newScoreGuest.textContent = scoreGuest
-   winner()
-}
-
-//////////////
-
 let foulsHome = 0
 let foulsGuest = 0
 
-const foulLightHome1 = document.getElementById("home-foul-light1")
-const foulLightHome2 = document.getElementById("home-foul-light2")
-const foulLightHome3 = document.getElementById("home-foul-light3")
-const foulLightHome4 = document.getElementById("home-foul-light4")
-const foulLightHome5 = document.getElementById("home-foul-light5")
+const foulLightsHome = [
+   document.getElementById("home-foul-light1"),
+   document.getElementById("home-foul-light2"),
+   document.getElementById("home-foul-light3"),
+   document.getElementById("home-foul-light4"),
+   document.getElementById("home-foul-light5")
+]
 
-const foulLightGuest1 = document.getElementById("guest-foul-light1")
-const foulLightGuest2 = document.getElementById("guest-foul-light2")
-const foulLightGuest3 = document.getElementById("guest-foul-light3")
-const foulLightGuest4 = document.getElementById("guest-foul-light4")
-const foulLightGuest5 = document.getElementById("guest-foul-light5")
+const foulLightsGuest = [
+   document.getElementById("guest-foul-light1"),
+   document.getElementById("guest-foul-light2"),
+   document.getElementById("guest-foul-light3"),
+   document.getElementById("guest-foul-light4"),
+   document.getElementById("guest-foul-light5")
+]
 
 ///
 
-function incrFoulsHome() {
-   foulsHome += 1
-   
-   if (foulsHome == 1) {
-      foulLightHome1.style.setProperty("--after-bg", "var(--color-foul-green)")
-      foulLightHome1.style.setProperty("--after-shadow", "var(--color-shadow-light)")
-   } else if (foulsHome == 2) {
-      foulLightHome2.style.setProperty("--after-bg", "var(--color-foul-green)")
-      foulLightHome2.style.setProperty("--after-shadow", "var(--color-shadow-light)")
-   } else if (foulsHome == 3) {
-      foulLightHome1.style.setProperty("--after-bg", "var(--color-foul-yellow)")
-      foulLightHome1.style.setProperty("--after-shadow", "var(--color-shadow-light)")
-      foulLightHome2.style.setProperty("--after-bg", "var(--color-foul-yellow)")
-      foulLightHome2.style.setProperty("--after-shadow", "var(--color-shadow-light)")
-      foulLightHome3.style.setProperty("--after-bg", "var(--color-foul-yellow)")
-      foulLightHome3.style.setProperty("--after-shadow", "var(--color-shadow-light)")
-   } else if (foulsHome == 4) {
-      foulLightHome1.style.setProperty("--after-bg", "var(--color-foul-orange)")
-      foulLightHome1.style.setProperty("--after-shadow", "var(--color-shadow-light)")
-      foulLightHome2.style.setProperty("--after-bg", "var(--color-foul-orange)")
-      foulLightHome2.style.setProperty("--after-shadow", "var(--color-shadow-light)")
-      foulLightHome3.style.setProperty("--after-bg", "var(--color-foul-orange)")
-      foulLightHome3.style.setProperty("--after-shadow", "var(--color-shadow-light)")
-      foulLightHome4.style.setProperty("--after-bg", "var(--color-foul-orange)")
-      foulLightHome4.style.setProperty("--after-shadow", "var(--color-shadow-light)")
-   } else if (foulsHome == 5) {
-      foulLightHome1.style.setProperty("--after-bg", "var(--color-bonus)")
-      foulLightHome2.style.setProperty("--after-bg", "var(--color-bonus)")
-      foulLightHome3.style.setProperty("--after-bg", "var(--color-bonus)")
-      foulLightHome4.style.setProperty("--after-bg", "var(--color-bonus)")
-      foulLightHome5.style.setProperty("--after-bg", "var(--color-bonus)")
-      foulLightHome5.style.setProperty("--after-shadow", "var(--color-shadow-light)")
+function incrFouls(team) {
+   if (team === "home") {
+      foulsHome++
+
+      if (foulsHome > 5) {
+         foulsHome = 5
+         return
+      }
+      updateFoulLights(team, foulsHome)
+   }
+   else if (team === "guest") {
+      foulsGuest++
+
+      if (foulsGuest > 5) {
+         foulsGuest = 5
+         return
+      }
+      updateFoulLights(team, foulsGuest)
    }
 }
 
-function incrFoulsGuest() {
-   foulsGuest += 1
-   
-   if (foulsGuest == 1) {
-      foulLightGuest5.style.setProperty("--after-bg", "var(--color-foul-green)")
-      foulLightGuest5.style.setProperty("--after-shadow", "var(--color-shadow-light)")
-   } else if (foulsGuest == 2) {
-      foulLightGuest4.style.setProperty("--after-bg", "var(--color-foul-green)")
-      foulLightGuest4.style.setProperty("--after-shadow", "var(--color-shadow-light)")
-   } else if (foulsGuest == 3) {
-      foulLightGuest4.style.setProperty("--after-bg", "var(--color-foul-yellow)")
-      foulLightGuest4.style.setProperty("--after-shadow", "var(--color-shadow-light)")
-      foulLightGuest5.style.setProperty("--after-bg", "var(--color-foul-yellow)")
-      foulLightGuest5.style.setProperty("--after-shadow", "var(--color-shadow-light)")
-      foulLightGuest3.style.setProperty("--after-bg", "var(--color-foul-yellow)")
-      foulLightGuest3.style.setProperty("--after-shadow", "var(--color-shadow-light)")
-   } else if (foulsGuest == 4) {
-      foulLightGuest4.style.setProperty("--after-bg", "var(--color-foul-orange)")
-      foulLightGuest4.style.setProperty("--after-shadow", "var(--color-shadow-light)")
-      foulLightGuest5.style.setProperty("--after-bg", "var(--color-foul-orange)")
-      foulLightGuest5.style.setProperty("--after-shadow", "var(--color-shadow-light)")
-      foulLightGuest3.style.setProperty("--after-bg", "var(--color-foul-orange)")
-      foulLightGuest3.style.setProperty("--after-shadow", "var(--color-shadow-light)")
-      foulLightGuest2.style.setProperty("--after-bg", "var(--color-foul-orange)")
-      foulLightGuest2.style.setProperty("--after-shadow", "var(--color-shadow-light)")
-   } else if (foulsGuest == 5) {
-      foulLightGuest1.style.setProperty("--after-bg", "var(--color-bonus)")
-      foulLightGuest2.style.setProperty("--after-bg", "var(--color-bonus)")
-      foulLightGuest3.style.setProperty("--after-bg", "var(--color-bonus)")
-      foulLightGuest4.style.setProperty("--after-bg", "var(--color-bonus)")
-      foulLightGuest5.style.setProperty("--after-bg", "var(--color-bonus)")
-      foulLightGuest5.style.setProperty("--after-shadow", "var(--color-shadow-light)")
+function updateFoulLights(team, count) {
+   const foulLights = team === "home" ? foulLightsHome : foulLightsGuest
+   const foulColors = {
+      1: "var(--color-foul-green)",
+      2: "var(--color-foul-green)",
+      3: "var(--color-foul-yellow)",
+      4: "var(--color-foul-orange)",
+      5: "var(--color-bonus)"
+   }
+   const foulShadows = "var(--color-shadow-light)"
+
+   // reset styles
+   foulLights.forEach( light => {
+      light.style.setProperty("--after-bg", "var(--color-bg-score)")
+      light.style.setProperty("--after-shadow", "var(--color-bg)")
+   })
+
+   if (count > 0 && count <= 5) {
+      const color = foulColors[count] || "var(--color-bg-score)"
+      for (let i = 0; i < count; i++) {
+         foulLights[i].style.setProperty("--after-bg", color)
+         foulLights[i].style.setProperty("--after-shadow", foulShadows)
+      }
    }
 }
 
 ///
 
-function resetFoulsHome() {
-   foulsHome = 0
-   
-   foulLightHome1.style.setProperty("--after-bg", "var(--color-bg-score)")
-   foulLightHome2.style.setProperty("--after-bg", "var(--color-bg-score)")
-   foulLightHome3.style.setProperty("--after-bg", "var(--color-bg-score)")
-   foulLightHome4.style.setProperty("--after-bg", "var(--color-bg-score)")
-   foulLightHome5.style.setProperty("--after-bg", "var(--color-bg-score)")
+function resetFouls(team) {
+   // Selecciona foulsHome/foulsGuest y foulLightsHome/foulLightsGuest
+   const foulLights = team === "home" ? foulLightsHome : foulLightsGuest
+   // Pone el contador a 0
+   const foulCount = team === "home" ? (foulsHome = 0) : (foulsGuest = 0)
+   // Resetea los indicadores a --color-bg-score y --color-bg
+   foulLights.forEach(light => {
+      light.style.setProperty("--after-bg", "var(--color-bg-score)")
+      light.style.setProperty("--after-shadow", "var(--color-bg)")
+   })
 }
 
-///
-
-function resetFoulsGuest() {
-   foulsGuest = 0
-   
-   foulLightGuest1.style.setProperty("--after-bg", "var(--color-bg-score)")
-   foulLightGuest2.style.setProperty("--after-bg", "var(--color-bg-score)")
-   foulLightGuest3.style.setProperty("--after-bg", "var(--color-bg-score)")
-   foulLightGuest4.style.setProperty("--after-bg", "var(--color-bg-score)")
-   foulLightGuest5.style.setProperty("--after-bg", "var(--color-bg-score)")
-}
 
 //////////////
 
@@ -201,26 +149,23 @@ function newGame() {
    newScoreHome.textContent = 0
    playerHome.style.textShadow = "none"
    playerHome.style.setProperty("--before-visibility", "hidden")
-   resetFoulsHome()
+   resetFouls("home")
    
    scoreGuest = 0
    newScoreGuest.textContent = 0
    playerGuest.style.textShadow = "none"
    playerGuest.style.setProperty("--before-visibility", "hidden")
-   resetFoulsGuest()
+   resetFouls("guest")
 }
 
-document.getElementById("incr-home1").addEventListener("click", incrHome1)
-document.getElementById("incr-home2").addEventListener("click", incrHome2)
-document.getElementById("incr-home3").addEventListener("click", incrHome3)
-document.getElementById("incr-guest1").addEventListener("click", incrGuest1)
-document.getElementById("incr-guest2").addEventListener("click", incrGuest2)
-document.getElementById("incr-guest3").addEventListener("click", incrGuest3)
+document.getElementById("incr-home1-btn").addEventListener("click", () => incrementScore("home", 1))
+document.getElementById("incr-home2-btn").addEventListener("click", () => incrementScore("home", 2))
+document.getElementById("incr-home3-btn").addEventListener("click", () => incrementScore("home", 3))
+document.getElementById("incr-guest1-btn").addEventListener("click", () => incrementScore("guest", 1))
+document.getElementById("incr-guest2-btn").addEventListener("click", () => incrementScore("guest", 2))
+document.getElementById("incr-guest3-btn").addEventListener("click", () => incrementScore("guest", 3))
 
-document.getElementById("home-foul-btn").addEventListener("click", incrFoulsHome)
-document.getElementById("guest-foul-btn").addEventListener("click", incrFoulsGuest)
-
-// document.getElementById("score-home").addEventListener("click", scoreHome)
-// document.getElementById("score-guest").addEventListener("click", scoreGuest)
+document.getElementById("home-foul-btn").addEventListener("click", () => incrFouls("home"))
+document.getElementById("guest-foul-btn").addEventListener("click", () => incrFouls("guest"))
 
 document.getElementById("new-game-btn").addEventListener("click", newGame)
